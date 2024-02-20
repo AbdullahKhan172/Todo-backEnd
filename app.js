edb779de225193dc90a51bf35c0500b68f01013c
Abdullah Khan <iiabdullahii.khan@gmail.com>
2024-02-20 20:55:57 +0500

    some packages added

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const todoRouter = require("./routes/todoRoutes");
 const userRouter = require("./routes/userRouter");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+var cors = require("cors");
+const corsOptions = {
+  origin: "",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use((req, res, next) => {
   console.log("Hello from the middle ware");
   next();
